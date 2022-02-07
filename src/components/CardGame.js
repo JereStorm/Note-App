9c4d6397d34bd4549a9e0d54856bfe5e3a01dd97
Jeremias Storm <jerestorm@gmail.com>
2022-02-06 22:31:05 -0300

    se agregan imagenes

--- src/components/CardGame.js
+++ src/components/CardGame.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import burako from '../imagenes/burako.jpg';
+import truco from '../imagenes/truco.jpg';
+import generala from '../imagenes/generala.jpeg';
 
-const CardGame = () => {
+const CardGame = ({game}) => {
+
+    let img = 0;
+
+    switch(game.title){
+        case 'truco':
+        img = truco;
+        break;
+        case 'burako':
+        img = burako;
+        break;
+        case 'generala':
+        img = generala;
+        break;
+    }
     return(
-        <div className='card m-2 border-info'>
+        <div className='card m-2 tarjeta'>
             <div className='card-head'>
-                <h1 className='card-title'>GameName</h1>
+                <h1 className='card-title'>{game.title}</h1>
             </div>
 
-            {/* IMAGEN DEL JUEGO */}
+            <img src={img}></img>
 
-            <div className='card-footer bg-dark d-flex justify-content-center align-items-end"'>
-            <span class="btn btn-dark">Jugadores: </span>
+            <div className='card-end bg-dark tarjeta_footer'>
+                <span class="btn btn-dark">team: </span>
                 <div class="btn-group gap-2" role="group">
-
-                    <span><a class="btn btn-dark">2</a></span>
-                    <span><a class="btn btn-dark">4</a></span>
-                    <span><a class="btn btn-dark">6</a></span>
-
+                    
+                    <span><a class="btn btn-dark">{ game.team }</a></span>
                 </div>
             </div>
         </div>
--- src/components/NoteGames.js
+++ src/components/NoteGames.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import CardGame from './CardGame';
 
+
+const games = [
+    {
+        title: 'truco',
+        team: 2,
+        counter: 30,
+        img: 'truco.jpg',
+    },
+    {
+        title: 'generala',
+        team: 'any',
+        counter: false,
+        img: 'truco.jpg',
+    },
+    {
+        title: 'burako',
+        team: 2,
+        counter: 3000,
+        img: 'burako.jpg',
+    },
+];
+
 const NoteGames = () => {
     return(
         <section className='box-games d-flex flex-column align-items-center mt-5'>
             <h2 className='center mb-5'>Aqui puede seleccionar el Juego:</h2>
-            <div className='d-flex flex-wrap justify-content-center'>
-                <CardGame />
-                <CardGame />
-                <CardGame />
-                <CardGame />
+            <div className='d-flex flex-wrap justify-content-center tarjeta_container'>
+                {games.map( game => <CardGame game={game}/>)}
             </div>
         </section>
     );
